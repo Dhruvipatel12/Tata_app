9f285606c3620c2a791f369898ed786339879bb3
SrushtiShah26 <srushtisanskartechnolab13@gmail.com>
2023-01-16 10:24:36 +0545

    Wrote the code for making fields read-only.

--- tata_app/public/js/item.js
+++ tata_app/public/js/item.js
@@ -5,18 +5,18 @@ frappe.ui.form.on('Item', {
 		// var vehicle_no=cur_frm.doc.license
 		var vehicle_no=cur_frm.doc.register_no
 		if (cur_frm.doc.rto_register=="Registered"){
-			console.log("Hello this is fetch data function")
+			// console.log("Hello this is fetch data function")
 			if (license1 == null)
 			{
-				console.log("Hello Null ")
+				// console.log("Hello Null ")
 				fetch_api(frm, vehicle_no)
 			}
 			else{
-				console.log("Hello Not Null")
+				// console.log("Hello Not Null")
 			}
 		}
 		else{
-			console.log("Not Register Person")
+			// console.log("Not Register Person")
 		}
 	},
 });
@@ -27,7 +27,7 @@ function fetch_api(frm,vehicle_no){
 		args: {'vehicle_no':vehicle_no},
 		callback: function(r) {
 			if (r.message == 'error'){
-				console.log("Data Are Not Inserted Successfully !!")
+				// console.log("Data Are Not Inserted Successfully !!")
 				frappe.validated = false;
 				frappe.throw("Wrong Vehicle Number Please Check and Type Again....")
 			}
@@ -80,32 +80,81 @@ function fetch_api(frm,vehicle_no){
 				var seating_capacity=main[22]
 				var permanent_address=main[23]
 				var norms_type=main[24]
+				cur_frm.set_df_property("license1", "read_only",1);
 				frm.set_value("license1",license)
+				
+
+				cur_frm.set_df_property("registration_date", "read_only",1);
 				frm.set_value("registration_date",registration_date)
+
 				frm.set_value("fuel_type",fuel_type)
+				cur_frm.set_df_property("fuel_type", "read_only",1);
+
+				frm.set_value("vehicle_color",fuel_type)
+				cur_frm.set_df_property("vehicle_color", "read_only",1);
+				
 				frm.set_value("present_address",present_address)
+				cur_frm.set_df_property("present_address", "read_only",1);
 
 				frm.set_value("full_chassis",full_chassis)
+				cur_frm.set_df_property("full_chassis", "read_only",1);
+
 				frm.set_value("owner_name",owner_name)
+				cur_frm.set_df_property("owner_name", "read_only",1);
+
 				frm.set_value("engine",engine)
+				cur_frm.set_df_property("engine", "read_only",1);
+
 				frm.set_value("vehicle_class",vehicle_class)
+				cur_frm.set_df_property("vehicle_class", "read_only",1);
+
 				frm.set_value("maker_model",maker_model)
+				cur_frm.set_df_property("maker_model", "read_only",1);
+
 				frm.set_value("owner_count",owner_count)
+				cur_frm.set_df_property("owner_count", "read_only",1);
+
 				frm.set_value("insurance_date",insurance_date)
+				cur_frm.set_df_property("insurance_date", "read_only",1);
+
 				frm.set_value("insurer_name",insurer_name)
+				cur_frm.set_df_property("insurer_name", "read_only",1);
+
 				frm.set_value("insurance_policy_no",insurance_policy_no)
+				cur_frm.set_df_property("insurance_policy_no", "read_only",1);
+
 				frm.set_value("pollution",pollution)
+				cur_frm.set_df_property("pollution", "read_only",1);
+
 				frm.set_value("fitness_dt",fitness_dt)
+				cur_frm.set_df_property("fitness_dt", "read_only",1);
+
 				frm.set_value("is_blacklisted",is_blacklisted)
+				cur_frm.set_df_property("is_blacklisted", "read_only",1);
 
 				frm.set_value("model",model)
+				cur_frm.set_df_property("model", "read_only",1);
+
 				frm.set_value("financier_name",financier_name)
+				cur_frm.set_df_property("financier_name", "read_only",1);
+
 				frm.set_value("manufacturing_date",manufacturing_date)
+				cur_frm.set_df_property("manufacturing_date", "read_only",1);
+
 				frm.set_value("registration_authority",registration_authority)
+				cur_frm.set_df_property("registration_authority", "read_only",1);
+
 				frm.set_value("vehicle_weight",vehicle_weight)
+				cur_frm.set_df_property("vehicle_weight", "read_only",1);
+
 				frm.set_value("seating_capacity",seating_capacity)
+				cur_frm.set_df_property("seating_capacity", "read_only",1);
+
 				frm.set_value("permanent_address",permanent_address)
+				cur_frm.set_df_property("permanent_address", "read_only",1);
+
 				frm.set_value("norms_type",norms_type)
+				cur_frm.set_df_property("norms_type", "read_only",1);
 				// cur_frm.save();
 				frappe.show_alert({
 								message: __("Data Are Inserted Successfully !!"),
