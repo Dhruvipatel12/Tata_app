a5234cec26267e9934dcaf9b6ea1235b01a3335a
SrushtiShah26 <srushtisanskartechnolab13@gmail.com>
2023-01-27 12:48:17 +0545

    removed the css of call button from supplier

--- tata_app/public/js/supplier.js
+++ tata_app/public/js/supplier.js
@@ -1,54 +1,8 @@
-// frappe.ui.form.on('Supplier', {
-//     refresh(frm) {
-//          if(frappe.user.has_role("Vendor") != true){
-//         var user = frappe.session.user_email;
-//             frappe.call({
-//                     method: "user_number",
-//                     args: { 'user_number': user },
-//                 }).then(records => {
-//                     var num = records["message"];
-//                     // alert(num)
-//                     frm.add_custom_button(__('Click-To-Call'), function(){
-                
-//                         var agent_number = num
-//                         // alert(agent_number)
-//                         var a1=frm.doc.phone_no
-    
-//                             frappe.confirm(
-//                                 "Are you sure want To Call?" ,
-                  
-//                           function(){
-//                             // alert(a1)
-//                             if(a1==""){
-//                                 frappe.msgprint("you Don't Have Any Number so you can't connect call",'Error')
-//                             }
-//                             else{
-                                
-//                                 frappe.call({
-                                    
-//                                     method:"tata_app.api.click_to_call", 
-//                                     args: {'agent_number':agent_number,'destination_number':a1},
-//                                     callback: function(r) {
-//                                         // alert("done")
-//                                     }
-//                                 });
-//                             }	
-//                             }
-//                             )
-              
-//                       })
-    
-        
-//                 })  
-//          }
-            
-//         }
-//     })
 frappe.ui.form.on('Supplier', {
     refresh(frm) {
-        var element = document.querySelectorAll(".form-section")[2];
-		element.classList.add("section-no");
-		set_css(frm);
+        // var element = document.querySelectorAll(".form-section")[2];
+		// element.classList.add("section-no");
+		// set_css(frm);
         if(frappe.user.has_role("Vendor") != true){
             var user = frappe.session.user_email;
                 frappe.call({
@@ -117,22 +71,22 @@ frappe.ui.form.on('Supplier', {
         },
     })
 
-function set_css(frm){
-	console.log("set_css");
-	document.querySelectorAll("[data-fieldname = 'call1']")[1].style.backgroundColor = '#0275d8';
-	document.querySelectorAll("[data-fieldname = 'call1']")[1].style.marginTop = '27px';
-	document.querySelectorAll("[data-fieldname = 'call1']")[1].style.color = '#fff';
+// function set_css(frm){
+// 	console.log("set_css");
+// 	document.querySelectorAll("[data-fieldname = 'call1']")[1].style.backgroundColor = '#0275d8';
+// 	document.querySelectorAll("[data-fieldname = 'call1']")[1].style.marginTop = '27px';
+// 	document.querySelectorAll("[data-fieldname = 'call1']")[1].style.color = '#fff';
 	
-	document.querySelectorAll("[data-fieldname = 'call2']")[1].style.backgroundColor = '#0275d8';
-	document.querySelectorAll("[data-fieldname = 'call2']")[1].style.marginTop = '27px';
-	document.querySelectorAll("[data-fieldname = 'call2']")[1].style.color = '#fff';
+// 	document.querySelectorAll("[data-fieldname = 'call2']")[1].style.backgroundColor = '#0275d8';
+// 	document.querySelectorAll("[data-fieldname = 'call2']")[1].style.marginTop = '27px';
+// 	document.querySelectorAll("[data-fieldname = 'call2']")[1].style.color = '#fff';
 	
-	document.querySelectorAll("[data-fieldname = 'call3']")[1].style.backgroundColor = '#0275d8';
-	document.querySelectorAll("[data-fieldname = 'call3']")[1].style.marginTop = '27px';
-	document.querySelectorAll("[data-fieldname = 'call3']")[1].style.color = '#fff';
+// 	document.querySelectorAll("[data-fieldname = 'call3']")[1].style.backgroundColor = '#0275d8';
+// 	document.querySelectorAll("[data-fieldname = 'call3']")[1].style.marginTop = '27px';
+// 	document.querySelectorAll("[data-fieldname = 'call3']")[1].style.color = '#fff';
 
-	console.log("hello end css");
-}
+// 	console.log("hello end css");
+// }
 
 function click_call(frm,no){
         if(no==null)
